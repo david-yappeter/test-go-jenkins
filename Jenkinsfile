a5a9e257cba149da42e50a06f314b1f87a5a9a1d
David Yappeter <davidyap11les@gmail.com>
2021-08-17 17:48:10 +0700

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,12 +3,6 @@ def gv
 pipeline{
     agent any
 
-    parameters{
-        string(name: "PARAM_A", defaultValue: "0", description: "")
-        string(name: "PARAM_B", defaultValue: "0", description: "")
-        choice(name: "VERSION", choices:["1.1.0", "1.1.1", "1.1.2"], description: "")
-    }
-
     tools {
         go "Go 1.16"
     }
@@ -32,7 +26,7 @@ pipeline{
         stage("build"){
             steps {
                 echo "build application"
-                sh "go run . ${params.PARAM_A} ${params.PARAM_B}"
+                sh "CGO_ENABLED=0 go build ."
 
                 script {
                     echo gv.buildApp()
@@ -43,14 +37,7 @@ pipeline{
         stage("deploy") {
             steps {
                 echo "deploy application"
-                echo "deploying with ${params.VERSION}"
             }
-        }
-    }
-
-    post{
-        always{
-            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
         }
     }
 }
--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Add(a, b float64) float64 {
@@ -22,15 +25,67 @@ func Divide(a, b float64) float64 {
 	return a / b
 }
 
+const defaultPort = "8080"
+
 func main() {
-	args := os.Args[1:]
-	a, _ := strconv.ParseFloat(args[0], 64)
-	b, _ := strconv.ParseFloat(args[1], 64)
+	// args := os.Args[1:]
+	// a, _ := strconv.ParseFloat(args[0], 64)
+	// b, _ := strconv.ParseFloat(args[1], 64)
 
-	fmt.Println(Add(a, b))
-	fmt.Println(Decrease(a, b))
-	fmt.Println(Multiply(a, b))
-	fmt.Println(Divide(a, b))
+	// fmt.Println(Add(a, b))
+	// fmt.Println(Decrease(a, b))
+	// fmt.Println(Multiply(a, b))
+	// fmt.Println(Divide(a, b))
 
-	fmt.Println("Hello Go")
+	// fmt.Println("Hello Go")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	f, _ := os.Create("gin.log")
+	defer f.Close()
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	router := gin.Default()
+
+	router.GET("/add", func(c *gin.Context) {
+		params := c.Request.URL.Query()
+		a, _ := strconv.ParseFloat(params.Get("a"), 64)
+		b, _ := strconv.ParseFloat(params.Get("b"), 64)
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": Add(a, b),
+		})
+	})
+	router.GET("/substract", func(c *gin.Context) {
+		params := c.Request.URL.Query()
+		a, _ := strconv.ParseFloat(params.Get("a"), 64)
+		b, _ := strconv.ParseFloat(params.Get("b"), 64)
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": Decrease(a, b),
+		})
+	})
+	router.GET("/multiply", func(c *gin.Context) {
+		params := c.Request.URL.Query()
+		a, _ := strconv.ParseFloat(params.Get("a"), 64)
+		b, _ := strconv.ParseFloat(params.Get("b"), 64)
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": Multiply(a, b),
+		})
+	})
+	router.GET("/divide", func(c *gin.Context) {
+		params := c.Request.URL.Query()
+		a, _ := strconv.ParseFloat(params.Get("a"), 64)
+		b, _ := strconv.ParseFloat(params.Get("b"), 64)
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": Divide(a, b),
+		})
+	})
+
+	router.Run(":" + port)
 }
